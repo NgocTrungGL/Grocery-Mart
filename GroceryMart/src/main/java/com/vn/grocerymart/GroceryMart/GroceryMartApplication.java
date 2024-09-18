d4b09313f69b3768df8611538cfa55b82bbe14b6
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-09-18 11:06:30 +0700

    Add Backend

---
+++ GroceryMart/src/main/java/com/vn/grocerymart/GroceryMart/GroceryMartApplication.java
@@ -0,0 +1,13 @@
+package com.vn.grocerymart.GroceryMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryMartApplication.class, args);
+	}
+
+}
---
+++ GroceryMart/src/test/java/com/vn/grocerymart/GroceryMart/GroceryMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.grocerymart.GroceryMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
